086cd2f17152ef27bf07c3c127276d2bf0ee66ea
skycool <skycool.fx@gmail.com>
2012-03-01 00:43:08 +0800

    ignore2

--- RedCloth-4.2.8/ruby/1.9.1/gems/pygments.rb-0.1.3/lib/pygments.rb
+++
@@ -1,9 +0,0 @@
-# require 'pygments/c'
-require 'pygments/ffi'
-
-module Pygments
-  # include Pygments::C
-  include Pygments::FFI
-
-  extend self
-end
--- RedCloth-4.2.8/ruby/1.9.1/gems/pygments.rb-0.1.3/lib/pygments/version.rb
+++
@@ -1,3 +0,0 @@
-module Pygments
-  VERSION = '0.1.3'
-end
